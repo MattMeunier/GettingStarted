cadbad028bd42a3492ab80e47344e315486e6c53
matt.meunier <matt.meunier@gmail.com>
2016-11-10 23:02:10 +0100

    Appli console basique

--- NombreMystereConsole/Program.cs
+++ NombreMystereConsole/Program.cs
@@ -8,40 +8,60 @@ namespace NombreMystereConsole
 {
     class Program
     {
+        static string welcome = "Saisir un nombre entre 0 et 20 : ";
+        static string wrongInput = "Oups, saisie incorrecte... C'est à toi : ";
         static string messSup = "Plus grand";
         static string messInf = "Plus petit";
         static string messWin = "Bravo, tu as trouvé en ";
-
-        static Random rand = new Random();
+        static int iterator = 1;
+        static int input;
+        static int num;
 
         static void Main(string[] args)
         {
-            int num = rand.Next(0, 20);
+            num = new Random().Next(0, 20);
+            newGame();
+            Console.ReadKey();
+        }
+
+        static private void newGame()
+        {
+           input = picked();
+           test(input, num);
+        }
+
+        static private int picked()
+        {
+            Console.WriteLine(welcome);
+
             int input;
-            int iterator = 1;
-            
+            bool isNum = int.TryParse(Console.ReadLine(), out input);
+
+            while (isNum == false || input>20)
             {
-                Console.WriteLine("Saisir un nombre entre 0 et 20 : ");
-                input = int.Parse(Console.ReadLine());
+                Console.WriteLine(wrongInput);
+                newGame();
+            }
+            return input;
+        }
 
-                while (num != input)
+        static private void test(int input, int num)
+        {
+            while (num != input)
+            {
+                if (num < input)
                 {
-                    if (num < input)
-                    {
-                        Console.WriteLine(messSup);
-                    }
-                    else
-                    {
-                        Console.WriteLine(messSup);
-
-                    }
-                    iterator ++;
+                    Console.WriteLine(messInf);
+                }
+                else
+                {
+                    Console.WriteLine(messSup);
 
                 }
-                Console.WriteLine(messWin + iterator + " coups");
-                Console.ReadLine();
+                iterator++;
+                input = picked();
             }
-            
+            Console.WriteLine(messWin + iterator + " coups");
         }
     }
 }
