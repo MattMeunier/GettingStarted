9f47f00a97ab5a5ec5685df7309f2cf4e08f40a2
Matt.Meunier <Meunier@MEUNIER-PC-FIXE>
2016-11-10 10:51:31 +0100

    Init

--- SayMyName/Program.cs
+++ SayMyName/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SayMyName
 {
@@ -10,6 +6,7 @@ namespace SayMyName
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
