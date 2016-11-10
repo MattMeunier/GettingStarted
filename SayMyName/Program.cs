cb5fc9bcc26f5b26fc8b4a724bebd41c1a1fe347
Matt.Meunier <Meunier@MEUNIER-PC-FIXE>
2016-11-10 12:07:05 +0100

    fini !

--- SayMyName/Program.cs
+++ SayMyName/Program.cs
@@ -6,7 +6,35 @@ namespace SayMyName
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Saisissez votre nom : ");
 
+            // Saisie du nom par l'utilisateur et affiche message d'accueil
+            string name = Console.ReadLine();
+            Console.WriteLine("Bonjour " + name);
+
+            Console.WriteLine("Quand êtes-vous né(e) : ");
+
+            // Saisie année de naissance par l'utilisateur
+            string year = Console.ReadLine();
+
+            // Calcule l'age de l'utilisateur et affiche
+            int yearNum = int.Parse(year);
+            int age = DateTime.Now.Year - yearNum;
+            string mess;
+            if (age < 40)
+            {
+                mess = "ça va gamin ?";
+            } else if (age < 60) {
+                mess = "bientôt la quille !";
+            } else
+            {
+                mess = "en vacances à durée indéterminée !";
+            }
+
+            Console.WriteLine("Vous avez " + age.ToString() + " ans, " + mess);
+
+            // Attente saisie pour fermer console
+            Console.ReadLine();
         }
     }
 }
